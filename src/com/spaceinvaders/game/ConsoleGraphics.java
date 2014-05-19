79e9535361df9f0af5ca9b09a1e51204c10b35d2
Miroslav Zografski <mzografski@gmail.com>
2014-05-19 11:47:07 +0300

    Game Loop added in Main class and ConsoleGraphics class added. Test state.

---
+++ src/com/spaceinvaders/game/ConsoleGraphics.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.spaceinvaders.game;
+
+/**
+ *
+ * @author Toshiba
+ */
+class ConsoleGraphics {
+    public final static String ESC = "\033[";
+    public final static String BUILDING_BLOCK = "█";
+    private static int cols = 0;
+    private static int rows = 0;
+
+    public ConsoleGraphics(int initCols, int initRows){
+        cols = initCols;
+        rows = initRows;
+        drawFrame();   
+        restoreCursor();
+    }
+    public static void drawFrame(){
+        char hLine = '\u2500';
+        char vLine = '\u2502';
+        char topLeftCorner = '\u250C';
+        char topRightCorner = '\u2510';
+        char botLeftCorner = '\u2514';
+        char botRightCorner = '\u2518';
+        
+        for(int i = 0; i <= rows;i++){
+            for(int j=0; j<= cols; j++){
+                if(i == 0 || i == rows){
+                    if(i == 0){
+                        if(j == 0)                        
+                            System.out.print(topLeftCorner);
+                        else if (j == cols)
+                            System.out.print(topRightCorner+"\n");
+                        else
+                            System.out.print(hLine);                                                    
+                    }
+                    else if(i == rows){
+                        if(j == 0)                        
+                            System.out.print(botLeftCorner);
+                        else if (j == cols)
+                            System.out.print(botRightCorner+"\n");
+                        else
+                            System.out.print(hLine);
+                    }                    
+                    else{
+                        System.out.print(hLine);
+                    }
+                }else if(j == 0 || j== cols){
+                    if(j == cols)
+                        System.out.printf("%s%n",vLine);
+                    else
+                        System.out.printf("%s",vLine);
+                }else{
+                    System.out.print(" ");
+                }                
+            }
+        }
+        restoreCursor();
+    }    
+    
+    public static void setCols(int newCols){
+        cols = newCols;
+    }
+    
+    public static void setRows(int newRows){
+        rows = newRows;
+    }
+    
+    public static int getCols(){
+        return cols;
+    }
+    
+    public static int getRows(){
+        return rows;
+    }
+    
+    public static void darwRectangle(int row, int col, int width, int height){
+        if(row < 0) row = 0;
+        if(col < 0) col = 0;
+        if(height < 1) height = 0;
+        if(width < 1) width = 0;
+        for( int i = row; i < (row + height); i++){
+            for(int j = col; j < (col + width); j++){
+                drawAtPosition(i, j, BUILDING_BLOCK.charAt(0));
+            }
+        }
+    }
+    
+    public static void drawLine(int startRow, int startCol, int endRow, int endCol){
+        if(startRow < 0) startRow = 0;
+        if(startCol < 0) startCol = 0;
+        if(endRow < 0) endRow = 1;
+        if(endCol < 0) endCol = 1;
+        for (int i = startRow; i <= endRow; i++) {
+            for (int j = startCol; j < endCol; j++) {
+                drawAtPosition(i, j, BUILDING_BLOCK.charAt(0));
+            }
+        }
+    }
+    
+    public static void drawAtPosition(int row, int col, char whatToDraw) {                
+        for(int i =0; i < row; i++){
+            System.out.print(ESC+"B");
+        }
+        for(int j = 0; j < col;j++){
+            System.out.print(ESC + "C");
+        }
+        System.out.print(whatToDraw);
+        restoreCursor();
+    }
+    
+    public static void displayText(int row, int col, String text){
+        for(int i =0; i < row; i++){
+            System.out.print(ESC + "B");
+        }
+        for(int j = 0; j < col;j++){
+            System.out.print(ESC + "C");
+        }
+        System.out.print(text);
+    }
+
+    public static void clearCons(){
+        System.out.print(ESC + "2J");
+    }
+
+    private static void restoreCursor() {
+        System.out.print(ESC + "H");        
+    }
+}
--- src/com/spaceinvaders/game/Main.java
+++ src/com/spaceinvaders/game/Main.java
@@ -6,6 +6,8 @@
 
 package com.spaceinvaders.game;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Toshiba
@@ -16,7 +18,75 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Play A Game?");
+        Scanner input = new Scanner(System.in);
+        String answer =  input.nextLine();
+        if(answer.equalsIgnoreCase("Y")){
+            gameStart();
+        }else{
+            System.exit(0);
+        }
+                
     }
-    
+    private static void gameStart() {        
+        boolean runing = true;
+        int cycles = 1000;
+        int currentCycle = 0;
+        int fps = 0, lastFpsTime = 0;
+        long lastLoopTime = System.currentTimeMillis();
+        final int TARGET_FPS = 1;
+        final long OPTIMAL_TIME = 1000 / TARGET_FPS; 
+        //char input;
+        while(runing){          
+            
+            long now = System.currentTimeMillis();
+            long updateLength = now - lastLoopTime;
+            lastLoopTime = now;
+            double delta = (updateLength / ((double)OPTIMAL_TIME))*1000;
+            //User Input 
+//            Scanner c = new Scanner(System.in);
+//            if(c.hasNext()){
+//                input = c.next().charAt(0);
+//                //System.out.print(input);
+//            }                       
+            
+            // update the frame counter
+            lastFpsTime += updateLength;
+            fps++;
+            // update our FPS counter if a second has passed since
+            // we last recorded
+            if (lastFpsTime >= OPTIMAL_TIME)
+            {
+               render(delta, fps);
+               //System.out.println(ConsoleGraphics.ESC + "32m (FPS: "+fps+")" + ConsoleGraphics.ESC + "37m");
+               lastFpsTime = 0;
+               fps = 0;
+            }
+            //render(delta);           
+            
+            
+            if(currentCycle == cycles){
+                runing = false;
+                 System.out.println((char)8);
+            }
+        }
+        
+    }
+
+    private static void render(double delta, int fps) {
+        int startPosLine = 10;
+        ConsoleGraphics.clearCons();
+        ConsoleGraphics.setCols(78);
+        ConsoleGraphics.setRows(28);
+        ConsoleGraphics.drawFrame();
+        ConsoleGraphics.darwRectangle(1, 1, 1, 1);
+        ConsoleGraphics.drawAtPosition(2, 2, '\u250C');
+        ConsoleGraphics.drawAtPosition(2, 3, '\u2500');
+        ConsoleGraphics.drawAtPosition(2, 4, '\u2510');
+        ConsoleGraphics.drawAtPosition(3, 2, '\u2514');
+        ConsoleGraphics.drawAtPosition(3, 3, '\u2500');
+        ConsoleGraphics.drawAtPosition(3, 4, '\u2518');
+        ConsoleGraphics.drawLine(5, 10, 5, 20);
+        ConsoleGraphics.displayText(1, 3, " "+delta);
+    }
 }
